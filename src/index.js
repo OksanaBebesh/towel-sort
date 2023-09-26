0679d0d7184432e97fccb06b35abc57209078a30
OksanaBebesh <o.bebesh@itransition.com>
2023-09-26 21:20:28 +0200

    add: add sorting matrix by snake base

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    if (arguments.length === 0) {
+        return [];
+    }
+    let result = [];
+    for (let i = 0; i <= matrix.length - 1; i++) {
+        for (let j = 0; j <= matrix[i].length - 1; j++) {
+            let indexId = i % 2 === 0 ? j : matrix[i].length - j - 1;
+            if (matrix[i][indexId] !== undefined) {
+                result.push(matrix[i][indexId]);
+            }
+        }
+    }
+    return result;
+};
